09a2b83756f7c8ecc0d26a5b8a87c797575ed84f
MD <knowintelligentlife@gmail.com>
2026-03-21 01:14:51 -0400

    Update Push Commits

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,11 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <main className="mx-auto flex min-h-[calc(100vh-56px)] max-w-6xl items-center justify-center px-4 py-10">
-      <SignIn
+      <SignUp
         fallbackRedirectUrl="/templates"
-        signUpFallbackRedirectUrl="/templates"
+        signInFallbackRedirectUrl="/templates"
       />
     </main>
   );
