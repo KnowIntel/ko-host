cfbb082396ea87235d817f72a24a6f37ffdc2c02
MD <knowintelligentlife@gmail.com>
2026-08-10 00:08:30 -0400

    Update Push Commits

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -7,153 +7,152 @@ const faqCards = [
 {
   title: "Perfect Moment",
   image: "/designs/design-cards/design-card_engagement_announcement_perfect.webp",
-  className: "left-[160px] top-[680px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[160px] top-[380px] rotate-[-4deg] hidden 2xl:block",
 },
 {
   title: "Playful Baby Shower",
   image: "/designs/design-cards/design-card_baby_shower_playful.webp",
-  className: "left-[80px] top-[1080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[80px] top-[480px] rotate-[-7deg] hidden 2xl:block",
 },
 {
   title: "Algebra",
   image: "/designs/design-cards/design-card_learning_lab_algebra.webp",
-  className: "left-[140px] top-[1480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[140px] top-[580px] rotate-[-5deg] hidden 2xl:block",
 },
 {
   title: "Casual Family Reunion",
   image: "/designs/design-cards/design-card_family_reunion_casual.webp",
-  className: "left-[40px] top-[1880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[40px] top-[680px] rotate-[-8deg] hidden 2xl:block",
 },
 {
   title: "Digital Pulse",
   image: "/designs/design-cards/design-card_creator_link_hub_digital.webp",
-  className: "left-[130px] top-[2280px] rotate-[-6deg] hidden 2xl:block",
+  className: "left-[130px] top-[780px] rotate-[-6deg] hidden 2xl:block",
 },
 {
   title: "Birthday Blast",
   image: "/designs/design-cards/design-card_birthday_party_blast.webp",
-  className: "left-[50px] top-[2680px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[50px] top-[880px] rotate-[-4deg] hidden 2xl:block",
 },
 
 {
   title: "Professional Beta Testing",
   image: "/designs/design-cards/design-card_beta_testing_professional.webp",
-  className: "left-[150px] top-[3080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[150px] top-[980px] rotate-[-7deg] hidden 2xl:block",
 },
 {
   title: "Bible Book Club",
   image: "/designs/design-cards/design-card_book_club_bible.webp",
-  className: "left-[70px] top-[3480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[70px] top-[1080px] rotate-[-5deg] hidden 2xl:block",
 },
 {
   title: "Prestige Graduation",
   image: "/designs/design-cards/design-card_graduation_prestige.webp",
-  className: "left-[130px] top-[3880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[130px] top-[1180px] rotate-[-8deg] hidden 2xl:block",
 },
 {
   title: "Elegant Baby Shower",
   image: "/designs/design-cards/design-card_baby_shower_elegant.webp",
-  className: "left-[30px] top-[4280px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[30px] top-[1280px] rotate-[-4deg] hidden 2xl:block",
 },
 {
   title: "Direct Networking",
   image: "/designs/design-cards/design-card_enthusiast_networking_direct.webp",
-  className: "left-[120px] top-[4680px] rotate-[-6deg] hidden 2xl:block",
+  className: "left-[120px] top-[1380px] rotate-[-6deg] hidden 2xl:block",
 },
 {
   title: "Golden Birthday",
   image: "/designs/design-cards/design-card_birthday_golden.webp",
-  className: "left-[40px] top-[5080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[40px] top-[1480px] rotate-[-7deg] hidden 2xl:block",
 },
 
 {
   title: "Atomic Review",
   image: "/designs/design-cards/design-card_learning_lab_atomic.webp",
-  className: "left-[140px] top-[5480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[140px] top-[1580px] rotate-[-5deg] hidden 2xl:block",
 },
 {
   title: "Studio Link Hub",
   image: "/designs/design-cards/design-card_creator_link_hub_studio.webp",
-  className: "left-[60px] top-[5880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[60px] top-[1680px] rotate-[-8deg] hidden 2xl:block",
 },
 {
   title: "City Council",
   image: "/designs/design-cards/design-card_election_campaign_city.webp",
-  className: "left-[120px] top-[6280px] rotate-[-6deg] hidden 2xl:block",
+  className: "left-[120px] top-[1780px] rotate-[-6deg] hidden 2xl:block",
 },
 {
   title: "Game Night",
   image: "/designs/design-cards/design-card_game_night_roll.webp",
-  className: "left-[20px] top-[6680px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[20px] top-[1880px] rotate-[-4deg] hidden 2xl:block",
 },
 {
   title: "Forever Question",
   image: "/designs/design-cards/design-card_engagement_announcement_forever.webp",
-  className: "left-[110px] top-[7080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[110px] top-[1980px] rotate-[-7deg] hidden 2xl:block",
 },
 {
   title: "Warm Family Reunion",
   image: "/designs/design-cards/design-card_family_reunion_warm.webp",
-  className: "left-[30px] top-[7480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[30px] top-[2080px] rotate-[-5deg] hidden 2xl:block",
 },
 
 {
   title: "Fabulous Fitness",
   image: "/designs/design-cards/design-card_guided_tutorial_fabulous.webp",
-  className: "left-[130px] top-[7880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[130px] top-[2180px] rotate-[-8deg] hidden 2xl:block",
 },
 {
   title: "Tiny Treasure",
   image: "/designs/design-cards/design-card_baby_shower_treasure.webp",
-  className: "left-[50px] top-[8280px] rotate-[-6deg] hidden 2xl:block",
+  className: "left-[50px] top-[2280px] rotate-[-6deg] hidden 2xl:block",
 },
 {
   title: "Vehicle Listing",
   image: "/designs/design-cards/design-card_for_sale_by_owner_auto.webp",
-  className: "left-[110px] top-[8680px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[110px] top-[2380px] rotate-[-4deg] hidden 2xl:block",
 },
 {
   title: "Community Newsletter",
   image: "/designs/design-cards/design-card_community_alert_newsletter.webp",
-  className: "left-[10px] top-[9080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[10px] top-[2480px] rotate-[-7deg] hidden 2xl:block",
 },
 {
   title: "Showcase Networking",
   image: "/designs/design-cards/design-card_enthusiast_networking_showcase.webp",
-  className: "left-[100px] top-[9480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[100px] top-[2580px] rotate-[-5deg] hidden 2xl:block",
 },
 {
   title: "Grown Birthday",
   image: "/designs/design-cards/design-card_birthday_grown.webp",
-  className: "left-[20px] top-[9880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[20px] top-[2680px] rotate-[-8deg] hidden 2xl:block",
 },
 
 {
   title: "Canvas Link Hub",
   image: "/designs/design-cards/design-card_creator_link_hub_canvas.webp",
-  className: "left-[120px] top-[10280px] rotate-[-6deg] hidden 2xl:block",
+  className: "left-[120px] top-[2780px] rotate-[-6deg] hidden 2xl:block",
 },
 {
   title: "Spotlight Graduation",
   image: "/designs/design-cards/design-card_graduation_spotlight.webp",
-  className: "left-[40px] top-[10680px] rotate-[-4deg] hidden 2xl:block",
+  className: "left-[40px] top-[2880px] rotate-[-4deg] hidden 2xl:block",
 },
 {
   title: "Elaborate Crowdfunding",
   image: "/designs/design-cards/design-card_crowdfunding_elaborate.webp",
-  className: "left-[100px] top-[11080px] rotate-[-7deg] hidden 2xl:block",
+  className: "left-[100px] top-[2980px] rotate-[-7deg] hidden 2xl:block",
 },
 {
   title: "Playful Birthday",
   image: "/designs/design-cards/design-card_birthday_playful.webp",
-  className: "left-[0px] top-[11480px] rotate-[-5deg] hidden 2xl:block",
+  className: "left-[0px] top-[3080px] rotate-[-5deg] hidden 2xl:block",
 },
 {
   title: "Easter Church Event",
   image: "/designs/design-cards/design-card_church_event_easter.webp",
-  className: "left-[90px] top-[11880px] rotate-[-8deg] hidden 2xl:block",
+  className: "left-[90px] top-[3180px] rotate-[-8deg] hidden 2xl:block",
 },
-
 
 
 
