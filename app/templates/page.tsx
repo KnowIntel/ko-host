d63850768d3269d25858364f1b1da5097c09ca32
MD <knowintelligentlife@gmail.com>
2026-08-25 13:28:40 -0400

    Update Push Commits

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1366,7 +1366,7 @@ function scrollToTemplates() {
 <div className="mt-8">
 <div
   id="template-results-anchor"
-  className="-mt-[10px] h-[10px] xl:-mt-[10px] xl:h-[10px]"
+  className="-mt-[10px] h-[10px] xl:-mt-[5px] xl:h-[5px]"
   aria-hidden="true"
 />
 
