e0bc3094c799e45b821025aba71fe73428cd5d12
MD <knowintelligentlife@gmail.com>
2026-09-21 20:55:20 -0400

    Update Push Commits

--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -159,13 +159,13 @@ export function Nav() {
                 Sign in
               </button>
 
-              <button
-                type="button"
-                onClick={() => navigateTo("/templates")}
-                className={primaryButtonClass}
-              >
-                Get started
-              </button>
+<button
+  type="button"
+  onClick={() => navigateTo("/connect")}
+  className={primaryButtonClass}
+>
+  Connect
+</button>
             </>
           ) : !isSignedIn ? (
             <>
